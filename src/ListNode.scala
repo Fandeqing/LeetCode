420468692a38e0909445d6e52bae9f923011134a
wqlin <wqlin0@gmail.com>
2017-10-19 11:46:42 +0800

    Add  auxiliary constructor for ListNode

--- src/ListNode.scala
+++ src/ListNode.scala
@@ -3,6 +3,11 @@
   * Definition for singly-linked list.
   */
 class ListNode(var _x: Int = 0) {
+  def this(_x: Int, next: ListNode) = {
+    this(_x)
+    this.next = next
+  }
+
   var next: ListNode = _
   var x: Int = _x
 }
