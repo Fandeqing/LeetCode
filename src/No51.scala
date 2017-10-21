73fd004270149f6375ea75b1c9b912c88625eb1b
wqlin <wqlin0@gmail.com>
2017-10-21 21:06:57 +0800

    AC

---
+++ src/No51.scala
@@ -0,0 +1,27 @@
+/**
+  * Created by wqlin on 17-10-21 10:33.
+  */
+object No51 {
+  // Inspired by Programming in Scala(3rd edition) in page 369
+  def solveNQueens(n: Int): List[List[String]] = {
+    def placeNQueens(k: Int): List[List[String]] =
+      if (k == 0)
+        List(List())
+      else {
+        for {
+          queens <- placeNQueens(k - 1)
+          column <- 0 until n
+          if isSafe((k - 1, column), queens)
+        } yield
+          queens ::: List(("." * n).updated(column, 'Q'))
+      }
+
+    def isSafe(queen: (Int, Int), queens: List[String]): Boolean =
+      queens.zipWithIndex.forall(p => !inCheck(queen, p._2, p._1))
+
+    def inCheck(queen: (Int, Int), row: Int, raw: String): Boolean =
+      raw.zipWithIndex.forall(pair => pair._1 == '.' || (queen._2 == pair._2 || (queen._1 - row).abs == (queen._2 - pair._2).abs))
+
+    placeNQueens(n)
+  }
+}
