7c7434926f2782a233fb4ca00829782bda529be7
wqlin <wqlin0@gmail.com>
2017-10-25 14:38:46 +0800

    AC

---
+++ src/No110.scala
@@ -0,0 +1,22 @@
+/**
+  * Created by wqlin on 17-10-25 14:26.
+  */
+object No110 {
+  def isBalanced(root: TreeNode): Boolean = {
+    def isBalanced(root: TreeNode): (Boolean, Int) =
+      if (root == null)
+        (true, 0)
+      else {
+        val (lb, lh) = isBalanced(root.left)
+        val (rb, rh) = isBalanced(root.right)
+        if (!lb || !rb)
+          (false, 0)
+        else if ((lh - rh).abs > 1)
+          (false, 0)
+        else
+          (true, (lh max rh) + 1)
+      }
+
+    isBalanced(root)._1
+  }
+}
