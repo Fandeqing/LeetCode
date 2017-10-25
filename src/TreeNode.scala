a4a6530625b8396286662e613e945ce8a339d8a7
wqlin <wqlin0@gmail.com>
2017-10-25 23:11:03 +0800

    Factory method to create TreeNode

--- src/TreeNode.scala
+++ src/TreeNode.scala
@@ -14,3 +14,18 @@ class TreeNode(var _value: Int) {
   var left: TreeNode = _
   var right: TreeNode = _
 }
+
+object TreeNode {
+  def apply(values: Any*): TreeNode = {
+    val len = values.length
+    val nodes = values.map(value => if (value == null) null else new TreeNode(value.asInstanceOf[Int]))
+    nodes.zipWithIndex.foreach { case (node, index) =>
+      if (index * 2 + 1 < len) node.left = nodes(index * 2 + 1)
+      if (index * 2 + 2 < len) node.right = nodes(index * 2 + 2)
+    }
+    if (nodes.nonEmpty)
+      nodes.head
+    else
+      null
+  }
+}
