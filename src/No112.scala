2d464f771c2431d8103a054f9196d9dd86c2c448
wqlin <wqlin0@gmail.com>
2017-11-03 13:21:53 +0800

    AC

---
+++ src/No112.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by wqlin on 17-11-3 13:00.
+  */
+object No112 {
+  def hasPathSum(root: TreeNode, sum: Int): Boolean = {
+    def loop(root: TreeNode, target: Int): Boolean =
+      if (root == null) false
+      else if (root.left == null && root.right == null) target + root.value == sum
+      else loop(root.left, target + root.value) || loop(root.right, target + root.value)
+
+    loop(root, 0)
+  }
+}
