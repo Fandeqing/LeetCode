38db803e745d5f76a74fc2618cd0bc7249348cf3
wqlin <wqlin0@gmail.com>
2017-11-13 09:20:08 +0800

    Change filter to withFilter

--- src/No1.scala
+++ src/No1.scala
@@ -4,7 +4,7 @@
 object No1 {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val m = nums.zipWithIndex.groupBy(_._1)
-    nums.zipWithIndex.filter(pair => {
+    nums.zipWithIndex.withFilter(pair => {
       val diff = target - pair._1
       m.contains(diff) && m(diff).exists(_._2 != pair._2)
     }).map(_._2)
