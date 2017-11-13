48093d4fab42fce86734abaebc47f55e3684a454
wqlin <wqlin0@gmail.com>
2017-11-13 09:24:35 +0800

    Avoid passing outer array variable to inner function

--- src/No88.scala
+++ src/No88.scala
@@ -3,23 +3,23 @@
   */
 object No88 {
   def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int): Unit = {
-    def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int, index: Int): Unit = {
+    def merge(m: Int, n: Int, index: Int): Unit = {
       if (index < 0) Unit
       else if (m >= 0 && n >= 0) {
         if (nums1(m) > nums2(n)) {
           nums1(index) = nums1(m)
-          merge(nums1, m - 1, nums2, n, index - 1)
+          merge(m - 1, n, index - 1)
         }
-        else {  // nums2(n) >= nums1(m)
+        else { // nums2(n) >= nums1(m)
           nums1(index) = nums2(n)
-          merge(nums1, m, nums2, n - 1, index - 1)
+          merge(m, n - 1, index - 1)
         }
       } else if (n >= 0) {
         nums1(index) = nums2(n)
-        merge(nums1, m - 1, nums2, n - 1, index - 1)
+        merge(m - 1, n - 1, index - 1)
       }
     }
 
-    merge(nums1, m - 1, nums2, n - 1, m + n - 1)
+    merge(m - 1, n - 1, m + n - 1)
   }
 }
