fe9d8cc7ec790918c62a6109fe302b067dc837aa
wqlin <wqlin0@gmail>
2017-11-25 22:40:47 +0800

    AC

---
+++ src/No145.scala
@@ -0,0 +1,23 @@
+/**
+  * Created by wqlin on 17-11-25 22:29.
+  */
+object No145 {
+  def postorderTraversal(root: TreeNode): List[Int] = {
+    def postorderTraversal(stack: List[TreeNode], res: List[Int]): List[Int] = stack match {
+      case h :: t =>
+        val r = h.value :: res
+        if (h.left != null && h.right != null)
+          postorderTraversal(h.right :: h.left :: t, r)
+        else if (h.left != null)
+          postorderTraversal(h.left :: t, r)
+        else if (h.right != null)
+          postorderTraversal(h.right :: t, r)
+        else
+          postorderTraversal(t, r)
+      case _ => res
+    }
+
+    if (root == null) Nil
+    else postorderTraversal(root :: Nil, Nil)
+  }
+}
