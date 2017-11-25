6d47d3d1529c86af6e2ff772fc45989d4eb956e0
wqlin <wqlin0@gmail>
2017-11-25 22:09:38 +0800

    AC

---
+++ src/No144.scala
@@ -0,0 +1,25 @@
+/**
+  * Created by wqlin on 17-11-25 21:42.
+  */
+object No144 {
+  def preorderTraversal(root: TreeNode): List[Int] = {
+    // stack + tail recursive
+    def preorderTraversal(stack: List[TreeNode], res: List[Int]): List[Int] = stack match {
+      case h :: t =>
+        val r = h.value :: res
+        if (h.right != null && h.left != null)
+          preorderTraversal(h.left :: h.right :: t, r)
+        else if (h.right != null)
+          preorderTraversal(h.right :: t, r)
+        else if (h.left != null)
+          preorderTraversal(h.left :: t, r)
+        else
+          preorderTraversal(t, r)
+
+      case _ => res.reverse
+    }
+
+    if (root == null) List.empty
+    else preorderTraversal(root :: Nil, Nil)
+  }
+}
