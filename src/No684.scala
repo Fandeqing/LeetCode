a82030368145152f8de817f7fd45e7adc9d8e053
wqlin <wqlin0@gmail>
2017-12-01 14:05:35 +0800

    AC

---
+++ src/No684.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by wqlin on 17-12-1 13:42.
+  */
+object No684 {
+  def findRedundantConnection(edges: Array[Array[Int]]): Array[Int] = {
+    val length = edges.length
+    val parent = (0 to length).toArray
+
+    def root(p: Int): Int =
+      if (p != parent(p)) {
+        parent(p) = parent(parent(p))
+        root(parent(p))
+      } else p
+
+    def connected(p: Int, q: Int) =
+      root(p) == root(q)
+
+    def union(p: Int, q: Int): Boolean = {
+      val rootP = root(p)
+      val rootQ = root(q)
+      if (rootP == rootQ) false
+      else {
+        parent(rootP) = rootQ
+        true
+      }
+    }
+
+    edges.view.filterNot {
+      case Array(p, q)
+      => union(p, q)
+    }.last
+  }
+}
