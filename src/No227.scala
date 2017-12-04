8949b88f92d6bccd7c12d94be9ea6edfa5071252
wqlin <wqlin0@gmail.com>
2017-12-04 14:21:51 +0800

    AC

---
+++ src/No227.scala
@@ -0,0 +1,49 @@
+import org.graalvm.compiler.lir.phases.LIRPhase.LIRPhaseStatistics
+
+/**
+  * Created by wqlin on 17-12-4 13:55.
+  */
+object No227 {
+  /**
+    * Because * and / and higher priority than + and -
+    * If we encounter + or -, we push it into operator stack for later evaluation
+    * Otherwise, we find the next operand and evaluate.
+    */
+  def calculate(s: String): Int = {
+    val length = s.length
+
+    def findNextNotDigit(i: Int): Int =
+      (i until length).find(!s(_).isDigit).getOrElse(length)
+
+    def findNextNotSpace(i: Int): Int =
+      (i until length).find(!s(_).isSpaceChar).getOrElse(length)
+
+    def evaluate(valueStack: List[Int], operatorStack: List[Char]): Int = operatorStack match {
+      case (op :: os) => valueStack match {
+        case (x :: y :: xs) => if (op == '+') evaluate((x + y) :: xs, os) else evaluate((x - y) :: xs, os)
+      }
+      case Nil => valueStack.head
+
+    }
+
+    def loop(i: Int, valueStack: List[Int], operatorStack: List[Char]): Int =
+      if (i >= length) evaluate(valueStack.reverse, operatorStack.reverse)
+      else if (s(i) == ' ') loop(i + 1, valueStack, operatorStack)
+      else if (s(i) == '+' || s(i) == '-') loop(i + 1, valueStack, s(i) :: operatorStack)
+      else if (s(i) == '*' || s(i) == '/') {
+        val start = findNextNotSpace(i + 1)
+        val end = findNextNotDigit(start)
+        val value = s.substring(start, end).toInt
+        valueStack match {
+          case x :: xs =>
+            if (s(i) == '*') loop(end, (x * value) :: xs, operatorStack) else loop(end, (x / value) :: xs, operatorStack)
+        }
+      } else {
+        val end = findNextNotDigit(i)
+        val value = s.substring(i, end).toInt
+        loop(end, value :: valueStack, operatorStack)
+      }
+
+    loop(0, List(), List())
+  }
+}
