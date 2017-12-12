5860915d63c78f0373a32b0aad9cdfbf1b486627
wqlin <wqlin0@gmail>
2017-12-12 23:54:52 +0800

    AC

--- src/No7.scala
+++ src/No7.scala
@@ -2,15 +2,17 @@
   * Created by wqlin on 17-12-12 23:04.
   */
 object No7 {
+  val threshold = 214748364
+
   def reverse(x: Int): Int = {
+
     @annotation.tailrec
     def loop(i: Int, acc: Int): Int =
       if (i == 0) if (x < 0) -acc else acc
       else {
-        val t = acc * 10
-        if (t / 10 != acc) 0 else loop(i / 10, t + i % 10)
+        if (acc > threshold) 0 else loop(i / 10, acc * 10 + i % 10)
       }
 
-    loop(x.abs, 0)
+    if (x == Int.MinValue) 0 else loop(x.abs, 0)
   }
 }
