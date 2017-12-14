0e5890e670f4e2a3ba0518ac276b6392ea026bed
wqlin <wqlin0@gmail>
2017-12-14 11:11:00 +0800

    AC

---
+++ src/No17.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by wqlin on 17-12-14 10:50.
+  */
+object No17 {
+  val m = Map('2' -> "abc", '3' -> "def", '4' -> "ghi", '5' -> "jkl", '6' -> "mno", '7' -> "pqrs", '8' -> "tuv", '9' -> "wxyz")
+
+  def letterCombinations(digits: String): List[String] =
+    if (digits.isEmpty) Nil
+    else {
+      digits.foldLeft(List("")) { (ls, digit) =>
+        m(digit).flatMap(char => ls.map(_ + char)).toList
+      }
+    }
+}
