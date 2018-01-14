7597e7abbc4f4ee7d23ffb045434544732d9c565
wqlin <wqlin0@gmail.com>
2018-01-14 10:01:52 +0800

    AC

---
+++ src/No62.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by wqlin on 18-1-14 09:26.
+  */
+object No62 {
+  def uniquePaths(m: Int, n: Int): Int = {
+    val grid = Array.fill(m, n)(0)
+    grid(0)(0) = 1
+    for {
+      i <- 0 until m
+      j <- 0 until n
+    } {
+      if (i - 1 >= 0) grid(i)(j) += grid(i - 1)(j)
+      if (j - 1 >= 0) grid(i)(j) += grid(i)(j - 1)
+    }
+    grid(m - 1)(n - 1)
+  }
+}
