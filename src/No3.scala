eac68b9ff8b4975c624078ce41848dc6d92e7652
wqlin <wqlin0@gmail.com>
2018-01-18 23:04:25 +0800

    AC

---
+++ src/No3.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by wqlin on 18-1-18 22:53.
+  */
+object No3 {
+  def lengthOfLongestSubstring(s: String): Int =
+    s.zipWithIndex.foldLeft((0, -1, Map[Char, Int]())) { case ((len, start_pos, map), (char, i)) =>
+      val last_pos = map.getOrElse(char, -1)
+      if (last_pos >= start_pos) (len max (i - last_pos), last_pos, map + (char -> i))
+      else (len max (i - start_pos), start_pos, map + (char -> i))
+    }._1
+}
