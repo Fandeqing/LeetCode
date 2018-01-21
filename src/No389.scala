7774fab59a34979c0202ec357279638931b65ecc
wqlin <wqlin0@gmail.com>
2018-01-21 11:42:27 +0800

    AC

---
+++ src/No389.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by wqlin on 18-1-21 11:10.
+  */
+object No389 {
+  def findTheDifference(s: String, t: String): Char = {
+    val m = s.foldLeft(Map.empty[Char, Int]) { (map, char) => map.updated(char, map.getOrElse(char, 0) + 1) }
+    t.foldLeft(m) { (map, char) => map.updated(char, map.getOrElse(char, 0) - 1) }.find(_._2 != 0).get._1
+  }
+}
