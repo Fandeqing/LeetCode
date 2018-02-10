25f86b2c7852a150c9c887e2531f5b33a80830e2
wqlin <wqlin0@gmail.com>
2018-02-10 13:04:07 +0800

    Use `value` instead of `_value` to access data in TreeNode

--- src/No100.scala
+++ src/No100.scala
@@ -4,7 +4,7 @@
 object No100 {
   def isSameTree(p: TreeNode, q: TreeNode): Boolean = {
     if (p != null && q != null)
-      p._value == q._value && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+      p.value == q.value && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
     else if (p != null || q != null)
       false
     else
