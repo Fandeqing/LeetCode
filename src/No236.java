0017b547b2af14bfdb397f08a995fe6fe87a2dd4
wqlin <wqlin0@gmail.com>
2018-03-20 21:51:29 +0800

    AC

---
+++ src/No236.java
@@ -0,0 +1,28 @@
+/**
+ * Created by wqlin on 18-3-20 21:45.
+ */
+public class No236 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==null||root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+            return root;
+        else if (left != null)
+            return left;
+        else if (right != null)
+            return right;
+        else return null;
+    }
+}
