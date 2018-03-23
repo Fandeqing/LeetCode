41a4679120304809a611df55cb7596d9dee46d35
wqlin <wqlin0@gmail.com>
2018-03-23 16:56:23 +0800

    AC

---
+++ src/No747.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by wqlin on 18-3-23 16:40.
+  */
+object No747 {
+  def dominantIndex(nums: Array[Int]): Int = {
+    var largestValueIndex = -1
+    val (firstLargest, secondLargest) = nums.zipWithIndex.foldLeft((Int.MinValue, Int.MinValue)) {
+      case ((fst, snd), (num, idx)) =>
+        if (num > fst) {
+          largestValueIndex = idx
+          (num, snd max fst)
+        } else if (num > snd)
+          (fst, num)
+        else (fst, snd)
+    }
+    if (firstLargest >= 2 * secondLargest) largestValueIndex else -1
+  }
+}
