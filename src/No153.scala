6a6b0d2201bd4b56548567c416bf600cdb77242e
wqlin <wqlin0@gmail.com>
2018-10-14 11:07:22 +0800

    AC

---
+++ src/No153.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by wqlin on 18-10-14 09:42.
+  */
+object No153 {
+  def findMin(nums: Array[Int]): Int = {
+
+    @annotation.tailrec
+    def findMin(lo: Int, hi: Int): Int =
+      if (lo >= hi) nums(lo)
+      else {
+        val m = lo + (hi - lo) / 2
+        if (nums(m) < nums(hi)) findMin(lo, m)
+        else findMin(m + 1, hi)
+      }
+
+    findMin(0, nums.length - 1)
+  }
+}
