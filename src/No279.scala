4da8a8b95e1c75a415293770b1ebe59325712c2c
wqlin <wqlin0@gmail.com>
2018-10-30 22:24:28 +0800

    AC

---
+++ src/No279.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by wqlin on 18-10-26 22:18.
+  */
+object No279 {
+  def numSquares(n: Int): Int =
+    if (n <= 1) n
+    else {
+      val dp = Array.fill(n + 1)(Int.MaxValue)
+      dp(0) = 0
+      dp(1) = 1
+      (2 to n).foreach { i =>
+
+        (1 until i).takeWhile(j => j * j <= i)
+          .foreach(j => dp(i) = Math.min(dp(i), dp(i - j * j) + 1))
+      }
+      dp(n)
+    }
+}
+
